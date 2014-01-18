d8cc55b73630cbb1943db1dd4fa972fe7d2538ea
longkai <im.longkai@gmail.com>
2014-01-18 09:12:16 +0800

    填写了从新浪那里获得的app信息，作为一个类存储。
    这些东西公开出来安全么=.=

---
+++ src/tingting/chen/util/Manifest.java
@@ -0,0 +1,25 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2014 longkai
+ * The software shall be used for good, not evil.
+ */
+package tingting.chen.util;
+
+/**
+ * 新浪微博客户端元数据。
+ *
+ * @author longkai
+ * @date 2013-01-18
+ */
+public class Manifest {
+
+	public static final String APP_KEY = "3195288873";
+
+	public static final String APP_SECRET = "f0bbaad564233b470d8f2850c08069cf";
+
+	/** 授权回调页 */
+	public static final String AUTH_REDIRECT_URI = "https://api.weibo.com/oauth2/default.html";
+
+	/** 取消授权回调页 */
+	public static final String UNAUTH_REDIRECT_URI = "https://api.weibo.com/oauth2/default.html";
+}
