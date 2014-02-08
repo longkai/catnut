8ec883d2f21e0bc673f9bcee29bd04e9a2534c83
longkai <im.longkai@gmail.com>
2014-02-09 01:30:56 +0800

    添加了Actionbar pull to refresh库
    下拉刷新的实现

--- src/tingting/chen/fragment/HomeTimelineFragment.java
+++ src/tingting/chen/fragment/HomeTimelineFragment.java
@@ -10,14 +10,14 @@ import android.app.ListFragment;
 import android.app.LoaderManager;
 import android.content.Loader;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.*;
 import android.widget.Toast;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import org.json.JSONObject;
 import tingting.chen.R;
 import tingting.chen.adapter.TweetAdapter;
 import tingting.chen.api.TweetAPI;
@@ -28,13 +28,18 @@ import tingting.chen.tingting.TingtingApp;
 import tingting.chen.tingting.TingtingProvider;
 import tingting.chen.tingting.TingtingRequest;
 import tingting.chen.util.TingtingUtils;
+import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
+import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshLayout;
+import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * 当前登录用户及其所关注用户的最新微博时间线
  *
  * @author longkai
  */
-public class HomeTimelineFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
+public class HomeTimelineFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor>,OnRefreshListener {
 
 	private static final String TAG = "HomeTimelineFragment";
 
@@ -43,6 +48,8 @@ public class HomeTimelineFragment extends ListFragment implements LoaderManager.
 	private RequestQueue mRequestQueue;
 	private Activity mActivity;
 	private TweetAdapter mAdapter;
+
+	private PullToRefreshLayout mPullToRefreshLayout;
 
 	@Override
 	public void onAttach(Activity activity) {
@@ -59,6 +66,18 @@ public class HomeTimelineFragment extends ListFragment implements LoaderManager.
 	}
 
 	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		ViewGroup viewGroup = (ViewGroup) view;
+		mPullToRefreshLayout = new PullToRefreshLayout(viewGroup.getContext());
+		ActionBarPullToRefresh.from(mActivity)
+			.insertLayoutInto(viewGroup)
+			.theseChildrenArePullable(android.R.id.list, android.R.id.empty)
+			.listener(this)
+			.setup(mPullToRefreshLayout);
+	}
+
+	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		menu.add(Menu.NONE, R.id.refresh, Menu.NONE, R.string.refresh)
 			.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
@@ -68,18 +87,8 @@ public class HomeTimelineFragment extends ListFragment implements LoaderManager.
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 			case R.id.refresh:
-				mRequestQueue.add(new TingtingRequest(
-					mActivity,
-					TweetAPI.homeTimeline(0, 0, 0, 0, 0, 0, 0),
-					new StatusProcessor.MyTweetsProcessor(),
-					null,
-					new Response.ErrorListener() {
-						@Override
-						public void onErrorResponse(VolleyError error) {
-							Toast.makeText(mActivity, error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-						}
-					}
-				));
+				mPullToRefreshLayout.setRefreshing(true);
+				onRefreshStarted(null);
 				break;
 			default:
 				break;
@@ -93,6 +102,12 @@ public class HomeTimelineFragment extends ListFragment implements LoaderManager.
 		setEmptyText(mActivity.getString(R.string.no_tweets));
 		setListAdapter(mAdapter);
 		getLoaderManager().initLoader(0, null, this);
+	}
+
+	@Override
+	public void onStop() {
+		mRequestQueue.cancelAll(TAG);
+		super.onStop();
 	}
 
 	@Override
@@ -129,4 +144,30 @@ public class HomeTimelineFragment extends ListFragment implements LoaderManager.
 	public void onLoaderReset(Loader<Cursor> loader) {
 		mAdapter.swapCursor(null);
 	}
+
+	@Override
+	public void onRefreshStarted(View view) {
+		mRequestQueue.add(new TingtingRequest(
+			mActivity,
+			TweetAPI.homeTimeline(0, 0, 0, 0, 0, 0, 0),
+			new StatusProcessor.MyTweetsProcessor(),
+			success,
+			error
+		)).setTag(TAG);
+	}
+
+	private Response.Listener success = new Response.Listener() {
+		@Override
+		public void onResponse(Object response) {
+			mPullToRefreshLayout.setRefreshComplete();
+		}
+	};
+
+	private Response.ErrorListener error = new Response.ErrorListener() {
+		@Override
+		public void onErrorResponse(VolleyError error) {
+			mPullToRefreshLayout.setRefreshComplete();
+			Toast.makeText(mActivity, error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+		}
+	};
 }
