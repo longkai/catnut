8ef60d896b1eaae7250aebb23255877b4c38d99f
longkai <im.longkai@gmail.com>
2014-03-19 10:43:35 +0800

    create zhihu plugin package.

---
+++ src/org/catnut/plugin/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2014 longkai
+ * The software shall be used for good, not evil.
+ */
+
+/**
+ * 插件，所有的插件均放置在这个目录下，并且以自己的一个独立目录作为命令空间
+ */
+package org.catnut.plugin;
